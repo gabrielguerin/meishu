a348c1a8389a5df4b9f549bd15ca4c0bc93a1108
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-09-26 18:03:42 +0200

    Generate Artist scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  # Admin routes
   devise_for :admins, path: 'admins', controllers: {
     sessions: 'admins/sessions'
   }
+
+  # Artists routes
   devise_for :artists, path: 'artists', controllers: {
     sessions: 'artists/sessions'
   }
+  resources :artists, only: %i[index show]
 
   # Defines the root path route ("/")
   root 'pages#show', page: 'home'
