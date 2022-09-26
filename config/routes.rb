91a8591ff26747c0d0bb31098cd084f96f4c28f3
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-09-26 17:51:11 +0200

    Name devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :artists
+  devise_for :admins, path: 'admins'
+  devise_for :artists, path: 'artists'
   resources :pages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
