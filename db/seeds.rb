03a80f783af5a1d6afeb834f282911a627829b21
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-09-27 17:55:36 +0200

    Seed artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Use faker to seed
+require 'faker'
+
+# Delete everything
+[Artist, Admin].each(&:delete_all)
+
+# Create artists
+20.times do
+  first_name = Faker::Name.unique.first_name
+  last_name = Faker::Name.unique.last_name
+  Artist.create!(
+    first_name:,
+    last_name:,
+    alternate_name: Faker::FunnyName.two_word_name,
+    email: "#{first_name}.#{last_name.parameterize(separator: '_')}@#{Faker::Internet.domain_name}".downcase,
+    telephone: Faker::Base.with_locale('fr') { Faker::PhoneNumber.cell_phone_with_country_code },
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraphs(number: 1),
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
