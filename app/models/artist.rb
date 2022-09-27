4a02f027e1d1bd00ae93098281dd34f702f5f2f5
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-09-27 19:06:33 +0200

    Add connection to Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,6 @@ class Artist < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :creative_works, dependent: :destroy
 end
