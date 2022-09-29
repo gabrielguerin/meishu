f24e489d98251ae9175823cbff79ac9529a96146
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-09-29 18:23:41 +0200

    Add full name method

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,4 +5,36 @@ class Artist < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :creative_works, dependent: :destroy
+
+  has_one_attached :avatar
+
+  # Add default avatar
+  after_commit :add_default_avatar, on: %i[create update]
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize: '150x150!').processed
+    else
+      image_url('artists/default_profile_picture.png')
+    end
+  end
+
+  # Full name
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(
+        Rails.root.join('app', 'assets', 'images', 'artists', 'default_profile_picture.png')
+      ),
+      filename: 'default_profile_picture.png',
+      content_type: 'image/png'
+    )
+  end
 end
