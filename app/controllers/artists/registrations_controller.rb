4f8f2d742133d35145198d5ca6f82c6bd4f3f39e
Gabriel GUERIN <gabriel.gueringg@gmail.com>
2022-10-07 15:40:43 +0200

    Add custom parameters to devise artists registration

--- app/controllers/artists/registrations_controller.rb
+++ app/controllers/artists/registrations_controller.rb
@@ -43,14 +43,18 @@ class Artists::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[avatar first_name last_name alternate_name description telephone address
+                                               url])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[avatar first_name last_name alternate_name description telephone address
+                                               url])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
